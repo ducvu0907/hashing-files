a72fce8aa586fe183b6245d3648d81040384d8fb
ducvu <ducvu973xd@gmail.com>
2024-04-14 02:07:37 +0700

    got correct output by reversing list

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import hashlib
 import os
 
-# testing
+# input file
 file_path = "birthday.mp4"
 
-def calculate_chunk_hash(file_path, chunk_size=1024):
-  pass
+def chunk_hash(chunk_data):
+  hash_object = hashlib.sha256()
+  hash_object.update(chunk_data)
+  return hash_object.digest()
 
 def verify_chunk(file_path, chunk_index, expected_hash, chunk_size=1024):
   pass
@@ -16,15 +18,40 @@ def read_chunks(file_object, chunk_size=1024):
     if not data:
       break
     yield data
+    
+def read_chunks_reversed(file_path, chunk_size=1024):
+  with open(file_path, "rb") as file:
+    file.seek(0, 2)
+    file_size = file.tell()
+    remaining_bytes = file_size
+    while remaining_bytes > 0:
+      start_pos = max(0, file_size - chunk_size)
+      bytes_to_read = min(remaining_bytes, chunk_size)
+      file.seek(start_pos)
+      chunk = file.read(bytes_to_read)
+      print(chunk)
+      remaining_bytes -= bytes_to_read
+      file_size = start_pos
 
 def bytes_to_bits(byte_data):
   return ''.join(format(byte, "08b") for byte in byte_data)
 
-def read_file(file_path):
+def bits_to_bytes(bits):
+  int_value = int(bits, 2)
+  num_bytes = (len(bits) + 7) 
+  return int_value.to_bytes(num_bytes, 'big')
+
+def hash_file(file_path):
+  data_list = []
   with open(file_path, "rb") as file:
     for chunk in read_chunks(file):
-      # TODO: implement hashing and checking each chunks
-      pass
-      
+      data_list.append(chunk)
+  return data_list
+
 if __name__ == "__main__":
-  pass
+  prev_hash = None
+  curr_hash = None
+  for data in hash_file(file_path)[::-1]:
+    curr_hash = chunk_hash(data) if not prev_hash else chunk_hash(data + prev_hash)
+    prev_hash = curr_hash
+  print(curr_hash.hex())
